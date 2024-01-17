7a8f5909cc596cba6946bfbabebab2ad82ae1458
bapoyyey <145336561+bapoyyey@users.noreply.github.com>
2024-01-18 06:34:53 +0700

    quiz1

---
+++ quiz.js
@@ -0,0 +1,46 @@
+// interactive.ts
+var readlineSync = require('readline-sync');
+function fizzBuzz() {
+    var fizzBuzzLimit = parseInt(readlineSync.question('Enter the limit for FizzBuzz: '));
+    console.log('\nFizzBuzz:');
+    for (var i = 1; i <= fizzBuzzLimit; i++) {
+        var output = '';
+        if (i % 3 === 0)
+            output += 'Fizz';
+        if (i % 5 === 0)
+            output += 'Buzz';
+        console.log(output || i);
+    }
+}
+function isPalindrome(str) {
+    var cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    var reversedStr = cleanStr.split('').reverse().join('');
+    return cleanStr === reversedStr;
+}
+function palindrome() {
+    var testString = readlineSync.question('Enter a string to check for palindrome: ');
+    console.log("Is \"".concat(testString, "\" a palindrome? ").concat(isPalindrome(testString)));
+}
+function mainMenu() {
+    while (true) {
+        console.log('\nMain Menu:');
+        console.log('1. FizzBuzz');
+        console.log('2. Palindrome');
+        console.log('3. Exit');
+        var choice = readlineSync.question('Enter your choice (1-3): ');
+        switch (choice) {
+            case '1':
+                fizzBuzz();
+                break;
+            case '2':
+                palindrome();
+                break;
+            case '3':
+                console.log('Exiting the program. Goodbye!');
+                process.exit(0);
+            default:
+                console.log('Invalid choice. Please enter a number between 1 and 3.');
+        }
+    }
+}
+mainMenu();
